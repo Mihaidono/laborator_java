9d2b251754700acc90aeaa75d6e308968f5096e9
Teodor Lupascu <teodor.lupascu@countthings.com>
2021-12-09 15:52:46 +0200

    wip

--- src/TestClass.java
+++ src/TestClass.java
@@ -26,46 +26,6 @@ public class TestClass {
 	}
 
 	public static void main(String[] args) {
-		Settings.initApplication();
-
-		System.out.println(Settings.loadType);
-		IDataLoader dataLoader = Settings.loadType == LOAD_TYPE.HARDCODAT ? new HardcodatDataManager() : new FileDataManager();
-		Profesor[] profesors = dataLoader.createProfesorData();
-		for (Profesor p :
-				profesors) {
-			System.out.println(p);
-		}
-
-
-
-		/* Scanner scanner = new Scanner(System.in);
-		System.out.println("a =");
-		String a = scanner.next();
-		try {
-			int aCaNrIntreg = Integer.parseInt(a);
-			System.out.print("Am citit " + aCaNrIntreg);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} */
-		/* String propertyName = LOAD_TYPE.FILE.name();
-		System.out.print(propertyName);
-		ArrayList<Student> students = new ArrayList<>();
-		try( Scanner scanner = new Scanner(new File("studenti.csv"))) {
-			while(scanner.hasNextLine()) {
-				try {
-					Student s = readStudentForm(scanner.nextLine());
-					students.add(s);
-				} catch (Exception ex) {
-					System.out.println(ex.getMessage());
-				}
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		for( Student s: students) {
-			System.out.println(s);
-		}
-		*/
+		
 	}
 }
